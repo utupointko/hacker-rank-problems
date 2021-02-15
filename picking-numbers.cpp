2685ae6e68faadfe80869a16ccd84b2875a83511
Omurzak Keldibekov <komur.19991008@gmail.com>
2021-02-16 03:33:22 +0600

    Modified picking-numbers

--- picking-numbers.cpp
+++ picking-numbers.cpp
@@ -1,9 +1,15 @@
-int designerPdfViewer(vector<int> h, string word) {
-    string abc = "abcdefghijklmnopqrstuvwxyz";
-    int height = 0;
-    for(char c : word){
-        int temp = h[abc.find(c)];
-        height = (height < temp) ? temp : height;
+int pickingNumbers(vector<int> a) {
+    map<int, int> mp;
+    for(int i : a){
+        mp[i]++;
     }
-    return height * word.size();
+    int len = 0;
+    for(auto it1 = mp.begin(); it1 != mp.end(); it1++){
+        int cnt = it1->second;
+        for(auto it2 = next(it1, 1); it2 != mp.end(); it2++){
+            if(abs(it1->first-it2->first) <= 1) cnt += it2->second;
+        }
+        len = (cnt > len) ? cnt : len;
+    }
+    return len;
 }
